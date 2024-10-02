6331804eaf708cf656bcc0bd82be16bc40f985f7
PrafulShinde8 <126104190+PrafulShinde8@users.noreply.github.com>
2024-10-03 04:47:10 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -1,55 +1,3 @@
-// const express = require('express')
-// const cors = require('cors')
-// const {connect} = require('mongoose')
-// require('dotenv').config()
-
-// const userRoutes = require('./routes/userRoutes')
-// const postRoutes = require('./routes/postRoutes')
-
-// const app = express();
-// app.use(express.json());
-// app.use(express.urlencoded({extended: true}));
-// app.use(cors({credentials: true, origin: "http://localhost:3000"}));
-
-// app.use('/api/users', userRoutes);
-// app.use('/api/posts', postRoutes);
-// connect(process.env.MONGO_URI).then(app.listen(5000, () => console.log(`Server running on
-// port ${process.env.PORT}`))).catch(error => {console.log(error)})
-
-
-// const express = require('express');
-// const cors = require('cors');
-// const { connect } = require('mongoose');
-// require('dotenv').config();
-
-// const userRoutes = require('./routes/userRoutes');
-// const postRoutes = require('./routes/postRoutes'); // Ensure this is correct
-// const {notFound, errorHandler} = require('./middleware/errorMiddleware')
-// const app = express();
-// const PORT = process.env.PORT || 5000;
-
-// // Middleware setup
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
-
-// // Use routes
-// app.use('/api/users', userRoutes);
-// app.use('/api/posts', postRoutes); 
-
-// app.use(notFound)
-// app.use(errorHandler)
-// // Connect to MongoDB and start server
-// connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() => {
-//     console.log('Connected to MongoDB');
-//     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-//   })
-//   .catch((error) => {
-//     console.error('Error connecting to MongoDB:', error);
-//   });
-
-
 const express = require('express');
 const cors = require('cors');
 const { connect } = require('mongoose');
@@ -68,7 +16,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
 app.use(upload());
-app.use('/uploads', express.static(__dirname + '/uploads'))
+app.use('/uploads', express.static(__dirname + '/uploads'));
+
+// Add a root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the MERN Blog App API!');
+});
 
 // Use routes
 app.use('/api/users', userRoutes);
@@ -84,9 +37,9 @@ app.use(errorHandler);
 connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(process.env.PORT || 5000, () => console.log
-    (`Server running on port ${process.env.PORT}`));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
   });
+
