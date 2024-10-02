63fe6addb202df9c463810dd8d4849690473b859
PrafulShinde8 <126104190+PrafulShinde8@users.noreply.github.com>
2024-10-03 05:19:56 +0530

    index.js

--- server/index.js
+++ server/index.js
@@ -14,18 +14,22 @@ const PORT = process.env.PORT || 5000;
 // Middleware setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+
+// CORS configuration
+const frontendOrigin = process.env.FRONTEND_URL || "http://localhost:3000"; // Fallback to localhost during development
+app.use(cors({ credentials: true, origin: frontendOrigin }));
+
 app.use(upload());
 app.use('/uploads', express.static(__dirname + '/uploads'));
+
+// Add a root route
+app.get('/', (req, res) => {
+  res.send('Welcome to the MERN Blog App API!');
+});
 
 // Use routes
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
-
-// Welcome route
-app.get('/', (req, res) => {
-  res.send('Welcome to the MERN Blog App API!');
-});
 
 // Handle 404 errors
 app.use(notFound);
@@ -37,8 +41,7 @@ app.use(errorHandler);
 connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(process.env.PORT || 5000, () => console.log
-    (`Server running on port ${process.env.PORT}`));
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
   })
   .catch((error) => {
     console.error('Error connecting to MongoDB:', error);
