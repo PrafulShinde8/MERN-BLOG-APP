c60689fd3f927480f381069d0b4833ee30661d0d
PrafulShinde8 <126104190+PrafulShinde8@users.noreply.github.com>
2024-10-03 15:43:10 +0530

    PostAuthor.jsx

--- client/src/components/PostAuthor.jsx
+++ client/src/components/PostAuthor.jsx
@@ -1,45 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import PostItem from '../components/PostItem'
-import Loader from '../components/Loader'
-import {useParams} from 'react-router-dom'
+import React, {useEffect, useState} from 'react'
+import {Link} from 'react-router-dom'
 import axios from 'axios'
-const AuthorPosts = () => {
-  const [posts, setPosts] = useState([]);
-  const [isLoading, setIsLoading] = useState(false)
-  const {id} = useParams()
+import ReactTimeAgo from 'react-time-ago'
+import TimeAgo from 'javascript-time-ago'
+
+import en from 'javascript-time-ago/locale/en.json'
+import ru from 'javascript-time-ago/locale/ru.json'
+
+TimeAgo.addDefaultLocale(en)
+TimeAgo.addLocale(ru)
+
+
+const PostAuthor = ({authorID, createdAt}) => {
+ const [author, setAuthor] = useState({})
   useEffect(() => {
-    const fetchPosts = async () => {
-       setIsLoading(true);
-       try {
-        const response = await axios.get(`${process.env.REACT_APP_API_URL}/posts/users/${id}`);
-        setPosts(response?.data)
-       } catch (err) {
-        console.log(err)
-       }
-
-       setIsLoading(false)
+    const getAuthor = async () => {
+     try {
+      const response = await axios.get(`${process.env.REACT_APP_API_URL}/users/${authorID}`)
+     setAuthor(response?.data);
+    } catch (error) {
+      console.log(error)
+     }
     }
-    fetchPosts();
-  },[id])
-
-  if(isLoading) {
-    return <Loader/>
-  }
-return (
-
-  <section className="posts">
-      {posts.length > 0 ? <div className="container posts__container">
-      {
-          posts.map(({_id:id, thumbnail, category, title, description, creator, createdAt}) =>
-          <PostItem key={id} postID={id} thumbnail={thumbnail} category={category} 
-          title= {title} description={description} authorID={creator} createdAt={createdAt}/>)
-      }
-      </div> : <h2 className='center'>No posts found</h2>}
-   
-  </section>
-);
-
+    getAuthor();
+  },[])
+  return (
+   <Link to={`/posts/users/${authorID}`} className='post__author'>
+     <div className="post__author-avatar">
+     <img src={`${process.env.REACT_APP_API_URL}/uploads/${author?.avatar}`} alt="" />
+     </div>
+     <div className="post__author-details">
+         <h5>By: {author?.name}</h5>
+         <small><ReactTimeAgo date={new Date(createdAt)} locale='en-US'/></small>
+     </div>
+   </Link>
+  )
 }
 
-export default AuthorPosts
+export default PostAuthor
+
 
